5ab5cc599e9418fc37311c63505dbc1aeae363b0
LouisCR04 <louiskariuki04@gmail.com>
2023-11-30 21:12:45 +0300

    Task 13: Nodes with at least a child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_nodes - Counts the nodes with at least 1 child
+ *@tree: Pointer to the tree
+ *
+ * Return: No. of these nodes else 0
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t n = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	n = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
+
+	return (n);
+}
